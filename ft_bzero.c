b45bc4584e7a57c7cfec712c27dfdcc110405d85
Ciproterona <eroque-d@student.42sp.org.br>
2026-05-17 13:02:27 -0300

    feat(ft_bzero): add bzero code

---
+++ ft_bzero.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eroque-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/16 21:43:50 by eroque-d          #+#    #+#             */
+/*   Updated: 2026/05/16 22:06:53 by eroque-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	*ft_bzero(void *s, size_t n)
+{
+	size_t			index;
+	unsigned char	*ptr;
+
+	index = 0;
+	ptr = (unsigned char *)s;
+	while (index < n)
+	{
+		ptr[index] = 0;
+		index++;
+	}
+	return (s);
+}
