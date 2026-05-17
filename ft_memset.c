4964098a218939fb4ab8ee6027443f0ccace3a50
Emanuel Roque <eroque-d@student.42sp.org.br>
2026-05-17 13:01:54 -0300

    feat(ft_memset): add memset code

---
+++ ft_memset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eroque-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/16 21:43:50 by eroque-d          #+#    #+#             */
+/*   Updated: 2026/05/16 22:06:53 by eroque-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	*ft_memset(void *s, int x, size_t n)
+{
+	size_t			index;
+	unsigned char	*ptr;
+
+	index = 0;
+	ptr = (unsigned char *)s;
+	while (index < n)
+	{
+		ptr[index] = (unsigned char)x;
+		index++;
+	}
+	return (s);
+}
