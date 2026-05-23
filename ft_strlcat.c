afdc7062da9fc39e6ca7188937529af274fe8b89
Ciproterona <eroque-d@student.42sp.org.br>
2026-05-23 13:43:08 -0300

    style(Norme): correct style of the Norme

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: eroque-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 15:26:07 by eroque-d          #+#    #+#             */
-/*   Updated: 2026/05/18 19:47:19 by eroque-d         ###   ########.fr       */
+/*   Updated: 2026/05/23 13:37:41 by eroque-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 size_t	strlcat(char *dest, const char *src, size_t size)
 {
-    size_t	i;
+	size_t	i;
 	size_t	j;
 
 	i = 0;
