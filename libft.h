dbb4c554a2c42879425f7454df513971fa11e6ab
Ciproterona <eroque-d@student.42sp.org.br>
2026-05-23 16:44:31 -0300

    feat(library): add ft_calloc function

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: eroque-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/16 21:12:34 by eroque-d          #+#    #+#             */
-/*   Updated: 2026/05/23 14:17:32 by eroque-d         ###   ########.fr       */
+/*   Updated: 2026/05/23 16:14:21 by eroque-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int		ft_isprint(int c_isacii);
 int		ft_isascii(int c_isascii);
 size_t	ft_strlen(const char *str);
 void	*ft_memset(void *s, int c, size_t n);
+void	*ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
@@ -34,5 +35,6 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *str);
+void	*ft_calloc(size_t count, size_t size);
 
 #endif
