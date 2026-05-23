9c0d97ab9850c3d199bc1f81681f5dc6f5ae55bc
Ciproterona <eroque-d@student.42sp.org.br>
2026-05-23 14:04:44 -0300

    refactor(ft_memchr): change function name from `memchr` to `ft_memchr`

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,13 +6,13 @@
 /*   By: eroque-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 14:49:18 by eroque-d          #+#    #+#             */
-/*   Updated: 2026/05/23 13:36:24 by eroque-d         ###   ########.fr       */
+/*   Updated: 2026/05/23 13:59:39 by eroque-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-void	*memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t			i;
 	unsigned char	*sp;
