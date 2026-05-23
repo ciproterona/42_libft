4631118cf3a8066ecf28f1418737ec797043ced1
Ciproterona <eroque-d@student.42sp.org.br>
2026-05-23 13:44:02 -0300

    feat(ft_memchr): add memchr code

---
+++ ft_memchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eroque-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/22 14:49:18 by eroque-d          #+#    #+#             */
+/*   Updated: 2026/05/23 13:36:24 by eroque-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*sp;
+
+	sp = (unsigned char *)s;
+	i = 0;
+	while (n > 0)
+	{
+		if (*sp == (unsigned char)c)
+			return ((void *)sp);
+		sp++;
+		n--;
+	}
+	return (NULL);
+}
