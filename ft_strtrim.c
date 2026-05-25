69b98d80fd898a1d339316a20a7952158ad4ca17
Emanuel Roque <eroque-d@student.42sp.org.br>
2026-05-25 15:56:05 -0300

    feat(ft_strtrim): add ft_strtrim code

---
+++ ft_strtrim.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eroque-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/25 15:01:17 by eroque-d          #+#    #+#             */
+/*   Updated: 2026/05/25 15:43:44 by eroque-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+	char	*str;
+
+	if (s1 == NULL || set == NULL)
+		return (NULL);
+	start = 0;
+	end = ft_strlen(s1);
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	str = ft_substr(s1, start, end - start);
+	return (str);
+}
