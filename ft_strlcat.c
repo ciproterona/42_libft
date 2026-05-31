b8a4973f061f756e04492f5677b99cb42824cf4c
Ciproterona <eroque-d@student.42sp.org.br>
2026-05-31 14:20:31 -0300

    fix(strlcat): fix return value and logic to prevent buffer overflow

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,27 +6,35 @@
 /*   By: eroque-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 15:26:07 by eroque-d          #+#    #+#             */
-/*   Updated: 2026/05/23 13:37:41 by eroque-d         ###   ########.fr       */
+/*   Updated: 2026/05/31 14:12:16 by eroque-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	strlcat(char *dest, const char *src, size_t size)
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
+	size_t	dest_len;
+	size_t	src_len;
 	size_t	i;
 	size_t	j;
 
-	i = 0;
-	while (dest[i] != '\0')
-		i++;
+	dest_len = 0;
+	while (dest[dest_len] && dest_len < size)
+		dest_len++;
+	src_len = 0;
+	while (src[src_len])
+		src_len++;
+	if (size <= dest_len)
+		return (size + src_len);
+	i = dest_len;
 	j = 0;
-	while (src[j] != '\0' && size < i)
+	while (src[j] && (i + 1) < size)
 	{
 		dest[i] = src[j];
-		j++;
 		i++;
+		j++;
 	}
 	dest[i] = '\0';
-	return (dest);
+	return (dest_len + src_len);
 }
