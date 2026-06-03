688807bff3e6b8337160037b517bc677639e94fc
Emanuel Roque <eroque-d@student.42sp.org.br>
2026-06-03 20:35:42 -0300

    feat(ft_lstclear): add ft_lstclear code

---
+++ ft_lstclear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eroque-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/03 15:00:02 by eroque-d          #+#    #+#             */
+/*   Updated: 2026/06/03 15:14:35 by eroque-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*next_node;
+
+	if (!lst || !del)
+		return ;
+	while (*lst != NULL)
+	{
+		next_node = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = next_node;
+	}
+}
