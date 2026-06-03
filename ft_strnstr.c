4087338bddbc84a7c626fbe8d1abeea95e7a1333
Emanuel Roque <eroque-d@student.42sp.org.br>
2026-06-03 20:46:17 -0300

    fix(ft_strnstr): prevent premature loop termination on boundary match

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: eroque-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/23 14:05:38 by eroque-d          #+#    #+#             */
-/*   Updated: 2026/06/03 14:05:49 by eroque-d         ###   ########.fr       */
+/*   Updated: 2026/06/03 19:32:46 by eroque-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,19 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 
 	if (!big || !little)
 		return (NULL);
-	i = 0;
 	if (little[0] == '\0')
 		return ((char *)big);
+	i = 0;
 	while (big[i] != '\0' && i < len)
 	{
 		j = 0;
-		while (little[j] != '\0' && (i + j) < len && big[i + j] == little[j])
+		while (big[i + j] == little[j] && (i + j) < len)
+		{
+			if (little[j + 1] == '\0')
+				return ((char *)&big[i]);
 			j++;
-		if (little[j] == '\0')
-			return ((char *)&big[i]);
+		}
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
