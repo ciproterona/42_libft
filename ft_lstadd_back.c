58d562f93f4c3f688bd5a1bf6223efd93715f8cf
Emanuel Roque <eroque-d@student.42sp.org.br>
2026-06-03 14:25:09 -0300

    feat(ft_lstadd_back): add ft_lstadd_back code

---
+++ ft_lstadd_back.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eroque-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/03 10:54:41 by eroque-d          #+#    #+#             */
+/*   Updated: 2026/06/03 11:07:55 by eroque-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (!lst || !new)
+		return ;
+	if (!*lst)
+		*lst = new;
+	last = ft_lstlast(*lst);
+	last->next = new;
+}
